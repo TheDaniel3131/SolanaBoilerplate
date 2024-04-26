db1c9187b0a134a9877ee83635d7efcbb8bc2c62
zephrous <71692327+TheDaniel3131@users.noreply.github.com>
2024-04-26 12:40:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app). This repo serves as a template for the APUBCC Sparkathon 2023, which allows users to build and interact with the Solana Blockchain. The template is built using Next.js, Tailwind CSS, [Solutils](https://solutils.vercel.app/), and using the [Phantom Wallet](https://phantom.app/) to connect to Solana.
+
+## Try the app here: [Click Me](https://solanaboilerplate.vercel.app/)
+
+
+
---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import { Inter } from 'next/font/google'
+import { useWalletTokenBalance } from '@lndgalante/solutils';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import dynamic from 'next/dynamic'
+import Button from '@/components/Button'
+
+const inter = Inter({ subsets: ['latin'] })
+
+function Home() {
+
+  const { publicKey } = useWallet();
+  const { connection } = useConnection();
+ 
+  // solutils hooks
+  const { getWalletTokenBalance, result, status, error } = useWalletTokenBalance(publicKey, connection);
+ 
+  // handlers
+  function handleWalletBalanceRequest() {
+    getWalletTokenBalance('SOL');
+  }
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          APU Hackthletes Solana Hackfest 2024
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <WalletMultiButton />
+        </div>
+      </div>
+
+      <div className="relative flex flex-col justify-between place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <h1 className='font-bold'>This is a boilerplate for APU Hackthletes Solana Hackfest 2024</h1>
+          {/* if publicKey found, display button */}
+          {publicKey ? <div className='place-items-center grid mt-10'>
+          <Button onClick={handleWalletBalanceRequest}>Request Wallet Balance</Button>
+          {status === 'iddle' ? <p>Haven&apos;t requested any SOL balance yet</p> : null}
+          {status === 'loading' ? <p>Requesting your SOL balance tokens</p> : null}
+          {status === 'success' ? <p>We successfully got your balance: {result} SOL</p> : null}
+          {status === 'error' ? <p>{error}</p> : null}
+        </div> : null}
+        
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      
+      </div>
+    </main>
+  )
+}
+
+export default dynamic (() => Promise.resolve(Home), {ssr: false} )
